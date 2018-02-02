2bd77875010090771a3418e80232d73150c1da0f
whitefire0 <rickhallett@live.co.ukk>
2018-02-02 13:11:14 +0000

    temporarily moved file presence error checking. deleteNote basic functionality implemented

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+/**
+ * Implementation List:
+ * custom fileName - set default, list current files, change active file
+ */
+
 console.log('app.js initialised');
 
 const fs = require('fs');
@@ -8,11 +13,12 @@ const notes = require('./notes.js')
 const argv = yargs.argv;
 
 let command = argv._[0]
-console.log(`Command: ${command}`);
-console.log('Yargs:', argv);
+// console.log(`Command: ${command}`);
+// console.log('Yargs:', argv);
 
 if (command === 'add') {
-  notes.addNote(argv.title, argv.body);
+  let newNote = notes.addNote(argv.title, argv.body);
+  console.log((!newNote) ? `Note with title:\n${argv.title} already exists. Please try again.` : `Note created. \n Title: ${argv.title} \n Text: ${argv.body}`);
 } else if (command === 'list') {
   notes.getAll();
 } else if (command === 'get') {
